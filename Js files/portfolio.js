4c53855f1ae4ebd8768ee133b9f54d77d0fac7ce
abd-0412 <abdajees01@gmail.com>
2025-02-15 15:28:37 +0530

    Add files via upload

---
+++ Js files/portfolio.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById("menu-icon");
+    const navMenu = document.querySelector(".nav-menu");
+    menuIcon.addEventListener("click", function() {
+        navMenu.classList.toggle("active");
+    });
+});
